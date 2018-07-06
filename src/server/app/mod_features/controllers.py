6db5f2ccb16f686efedc1a1f5dce1d572a970dde
MFB <mburgess@comscore.com>
2018-07-06 06:21:43 -0400

    Create Added

--- src/server/app/mod_features/controllers.py
+++ src/server/app/mod_features/controllers.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify, make_response
+from flask import Blueprint, jsonify, make_response, request
 
-from app.mod_features.models import Feature
+from app.mod_features.models import Feature, query_all, create_feature
+
+import datetime
 
 mod_features = Blueprint('features', __name__, url_prefix='/v1/features')
 
@@ -8,13 +10,32 @@ mod_features = Blueprint('features', __name__, url_prefix='/v1/features')
 @mod_features.route('/', methods=['GET'])
 def get_all():
     print 'controller'
-    feature_query = Feature.query.all()
-    all_features = []
-    for c in feature_query:
-        all_features.append({"id": c.id, "title": c.title, "description": c.description, "client": c.client, "clien_priority": c.client_priority, "target_date": c.target_date, "product_area": c.product_area})
-    featuring_json_derulo = jsonify(all_features)
-    print "featuring_json_derulo"
-    print featuring_json_derulo
+    #feature_query = Feature.query.all()
+    featuring_json_derulo = jsonify(query_all())
     resp = make_response(featuring_json_derulo)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
+@mod_features.route('/', methods = ['POST'])
+def create():
+	
+	if not request.json or not 'title' in request.json:
+		abort(400)
+
+	f = Feature(title= request.json['title'], \
+		description= request.json.get('description', "None"), \
+		client= request.json.get('client', "None"), \
+		client_priority= request.json.get('client_priority', 0), \
+		target_date= datetime.datetime(\
+			request.json.get('target_date_year', datetime.date.today().year), \
+			request.json.get('target_date_month', datetime.date.today().month), \
+			request.json.get('target_date_day', datetime.date.today().day)), \
+		product_area= request.json.get('product_area', "None"))
+	print f
+	create_call = create_feature(f)
+
+	# Session = sessionmaker(bind=db_connect)
+	# session = Session()
+	# session.add(f)
+	# session.commit()
+	return create_call
--- src/server/app/mod_features/models.py
+++ src/server/app/mod_features/models.py
@@ -16,11 +16,29 @@ class Feature(db.Model):
     def __repr__(self):
         return '<Feature {}>'.format(self.title)
 
+def query_all():
+    # feature_query = list(self.query.all())
+    feature_query = Feature.query.all()
+    all_features = []
+    for c in feature_query:
+        all_features.append({"id": c.id, "title": c.title, "description": c.description, "client": c.client, "clien_priority": c.client_priority, "target_date": c.target_date, "product_area": c.product_area})
+    return all_features
+
+def create_feature(f):
+    try:
+        db.session.add(f)
+        db.session.commit()
+        return 'true', 201
+    except Exception as e:
+        print e
+        return 'An error was raised. This probably wasn\'t too helpful', 500
+
+
 def init_db():
     print "init_db"
 
     # db.create_all()
-    
+
     # db.session.add(Feature(title="t1", description="d1", client="c1", client_priority="1", target_date=datetime.datetime(2018, 3, 24, 2, 9, 12), product_area="p1"))
     # db.session.commit()
     # print Feature.query.all()
